d975caaca923774fed3cef41df22fcbb6bd54631
panven-ops <venizeleaspanos@gmail.com>
2026-06-11 13:43:37 +0300

    update: cors fix

--- backend/admin/admin_auth.py
+++ backend/admin/admin_auth.py
@@ -8,6 +8,9 @@ ADMIN_SECRET = os.getenv("ADMIN_SUPER_SECRET_KEY")
 
 def verify_admin(authorization: str = Header(None)):
 
+    if request.method == "OPTIONS":
+        return True
+
     if not authorization:
         raise HTTPException(status_code=401, detail="Missing authorization")
 
